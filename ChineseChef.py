9ab9cf73975a3069b6e06c3e8deae0930a44c2ad
unknown <carlos_valverdeb@hotmail.com>
2020-06-18 19:24:42 -0400

    first push

---
+++ ChineseChef.py
@@ -0,0 +1,7 @@
+from Chef import Chef
+
+class ChineseChef(Chef):
+    def make_fried_rice(self):
+        print("Thef chef makes fried rice")
+    def make_special_dish(self):
+            print("Thef chef makes orange chicken")
---
+++ app.py
@@ -0,0 +1,11 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+
+myChef=Chef()
+myChef.make_chicken()
+myChef.make_special_dish()
+
+myChineseChef=ChineseChef()
+myChineseChef.make_chicken()
+myChineseChef.make_fried_rice()
+myChineseChef.make_special_dish()
